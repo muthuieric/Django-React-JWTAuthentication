b9d56c677707a6a6cf9ffd1d3cbb97be5402cdc8
Eric <ericmuthuipatch22@gmail.com>
2024-01-09 21:49:45 +0300

    layout error

---
+++ frontend/src/containers/ResetPasswordConfirm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RestPasswordConfirm = () => {
+    return(
+        <div>
+            RestPasswordConfirm 
+        </div>
+    )
+};
+
+export default RestPasswordConfirm;
