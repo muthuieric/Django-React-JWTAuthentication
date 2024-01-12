7c45843dc84d412ed01af0a56f77e4a32a64a85f
Eric <ericmuthuipatch22@gmail.com>
2024-01-12 20:46:05 +0300

    styled using tailwind

--- frontend/src/Component/navbar.jsx
+++ frontend/src/Component/navbar.jsx
@@ -4,49 +4,88 @@ import { connect } from "react-redux";
 import { logout } from "../reducer/Actions";
 
 const Navbar = ({ logout, isAuthenticated }) => {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Mywebsite.co.id</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                    </li>
-                    { isAuthenticated? (
-                        <>
-                        <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" to="change/password/">Change Password</Link>
-                        </li>
-                        <li className="nav-item">
-                        <span className="nav-link active" aria-current="page" onClick={ logout } id="logout">Logout</span>
-                        </li>
-                        </>
-                    ): (
-                        <>
-                        <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" to="login/">Login</Link>
-                        </li>
-                        <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" to="signup/">Signup</Link>
-                        </li>
-                        </>
-                    ) }
-                    
-                </ul>
-                </div>
-            </div>
-        </nav>
-    )
-}
+  return (
+    <nav className="bg-primary">
+      <div className="container mx-auto">
+        <Link className="text-white text-xl font-bold" to="/">
+          VMS
+        </Link>
+        <button
+          className="lg:hidden px-2 py-1 text-white"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="hidden lg:flex lg:items-center lg:ml-auto">
+          <ul className="flex space-x-4">
+            <li>
+              <Link
+                className="text-white"
+                to="/"
+                activeClassName="font-bold"
+              >
+                Home
+              </Link>
+            </li>
+            {isAuthenticated ? (
+              <>
+                <li>
+                  <Link
+                    className="text-white"
+                    to="/change/password/"
+                    activeClassName="font-bold"
+                  >
+                    Change Password
+                  </Link>
+                </li>
+                <li>
+                  <span
+                    className="text-white cursor-pointer"
+                    onClick={logout}
+                    id="logout"
+                  >
+                    Logout
+                  </span>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link
+                    className="text-white"
+                    to="/login/"
+                    activeClassName="font-bold"
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="text-white"
+                    to="/signup/"
+                    activeClassName="font-bold"
+                  >
+                    Signup
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-const mapStateToProps = ( state ) => {
-    return {
-        isAuthenticated: state.AuthReducer.isAuthenticated
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.AuthReducer.isAuthenticated,
+  };
+};
 
-export default connect(mapStateToProps, { logout })(Navbar)
+export default connect(mapStateToProps, { logout })(Navbar);
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -3,16 +3,27 @@ import { connect } from "react-redux";
 import { refresh } from "../reducer/Actions";
 
 const Home = ({ refresh }) => {
-    return (
-        <div className="p-5 mb-4 bg-light rounded-3">
-            <div className="container-fluid py-5">
-                <h1 className="display-5 fw-bold">Welcome in mywebsite.co.id</h1>
-                <p className="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-                <button className="btn btn-primary btn-lg" type="button" onClick={ refresh }>Refresh</button>
-            </div>
+  return (
+    <div className="container mx-auto mt-12">
+      <div className="p-5 bg-light rounded-lg shadow-md">
+        <div className="py-5">
+          <h1 className="text-4xl font-bold">Welcome to mywebsite.co.id</h1>
+          <p className="mt-4 text-lg">
+            Using a series of utilities, you can create this jumbotron, just like
+            the one in previous versions of Bootstrap. Check out the examples
+            below for how you can remix and restyle it to your liking.
+          </p>
+          <button
+            className="mt-6 btn btn-primary btn-lg bg-blue-600 hover-bg-blue-800 "
+            type="button"
+            onClick={refresh}
+          >
+            Refresh
+          </button>
         </div>
-        
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default connect(null, { refresh })(Home);
