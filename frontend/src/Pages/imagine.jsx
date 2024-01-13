f1aff62a59bcbd0dca9a1f6791357b07b345b0ac
Eric <ericmuthuipatch22@gmail.com>
2024-01-13 16:35:42 +0300

    updated authentication in imagine page

--- frontend/src/Pages/imagine.jsx
+++ frontend/src/Pages/imagine.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+import { connect } from "react-redux";
 
-const imagine = () => {
+const Imagine = ({ isAuthenticated }) => {
+  // Check if the user is not authenticated, then redirect to the login page
+  if (!isAuthenticated && !localStorage.getItem("access")) {
+    return <Navigate to={"../login"} />;
+  }
+
   return (
     <div className="container mx-auto mt-12">
       <div className="p-5 bg-light rounded-lg shadow-md">
@@ -11,11 +18,16 @@ const imagine = () => {
             the one in previous versions of Bootstrap. Check out the examples
             below for how you can remix and restyle it to your liking.
           </p>
-        
         </div>
       </div>
     </div>
   );
 };
 
-export default imagine;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.AuthReducer.isAuthenticated,
+  };
+};
+
+export default connect(mapStateToProps)(Imagine);
